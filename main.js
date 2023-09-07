ce68cec3fa28f32d535a5ec5b9d46a81f6182910
Youssef | Developer <141073820+YousseeifIsmail@users.noreply.github.com>
2023-09-07 18:56:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// Menu
+let subMenu = document.getElementById("subMenu");
+function toggleMenu() {
+	subMenu.classList.toggle("accOpen");
+};
+
+// Account
+let navMenu = document.getElementById("navMenu");
+function toggleNavMenu() {
+	navMenu.classList.toggle("menuOpen");
+};
+
+// Poster
+let subPoster = document.getElementById("Poster");
+function togglePoster() {
+	subPoster.classList.toggle("PosterView");
+	console.log(11);
+};
+
+// Story
+let subStory = document.getElementById("story");
+function toggleStory() {
+	subStory.classList.toggle("storyView");
+	console.log(11);
+};
